9ad405eaa2b0f4b872c65297c72cf7f73f8e5b87
Kevin <LordTaitex@gmail.com>
2022-08-08 06:42:34 -0500

    Able to add questions to database.

--- setup.py
+++ setup.py
@@ -1,3 +1,13 @@
 from setuptools import setup
 
-# install Package via "python setup.py install"
+# install Package via "pip install ."
+# python setup.py install
+
+setup(
+    name="Sec Plus Question Prompt",
+    version="",
+    description="Prompts questions for the test.",
+    author="Kevin Lu",
+    packages=["src"],
+    install_requires=["pymongo", "PyPDF2"]
+)
